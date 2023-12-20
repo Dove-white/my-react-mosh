86d1077158ff87368d4ac0c25f11716f29019ed9
Dove-White <dugbazahredeemer@gmail.com>
2023-12-20 02:50:15 +0000

    added form using useForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import ExpandableText from "./Component/ExpandableText";
+import Form from "./Component/Form";
 
 function App() {
-  return (
-    <ExpandableText bgColor="dark">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium
-      provident qui, excepturi, modi dicta amet possimus corrupti animi beatae
-      deserunt assumenda nobis sunt. Animi repellendus molestiae recusandae
-      ullam quam distinctio delectus atque omnis voluptatem voluptate, dolores,
-      facere iure alias porro adipisci numquam cupiditate soluta harum
-      perferendis doloribus quos vitae! Minima saepe amet animi nisi
-      exercitationem quo mollitia sunt eligendi deserunt? Distinctio alias
-      itaque esse molestiae ipsam doloremque! Ratione minus tenetur nostrum
-      natus similique facere nobis dolores incidunt ullam. Nemo provident iure
-      distinctio. Quod tenetur repellendus sequi tempora expedita animi
-      excepturi placeat voluptatum rerum earum voluptas a, deleniti corporis,
-      temporibus aliquam.
-    </ExpandableText>
-  );
+  return <Form />;
 }
 
 export default App;
--- src/Component/Form.tsx
+++ src/Component/Form.tsx
@@ -1,22 +1,43 @@
-import React from "react";
+import { FieldValues } from "react-hook-form/dist/types";
+import { useForm } from "react-hook-form";
 
 const Form = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
   return (
-    <form>
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
-        </label>
-        <input id="name" type="text" className="form-control" />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="age" className="form-label">
-          Age
-        </label>
-        <input id="age" type="number" className="form-control" />
-      </div>
-      <button className="btn btn-primary p">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            {...register("name")}
+            id="name"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="age" className="form-label">
+            Age
+          </label>
+          <input
+            {...register("age")}
+            id="age"
+            type="number"
+            className="form-control"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary p">
+          Submit
+        </button>
+      </form>
+    </>
   );
 };
 
